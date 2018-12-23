b8146b35a4123d911e4292aeec4efe0396779612
Kitty <kitsumpang@gmail.com>
2018-12-23 18:54:09 +0000

    fixed imports

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
- from django.urls import path
- from .views import BookAPIView 
+from django.urls import path
+from .views import BookAPIView
 
 urlpatterns = [
 	path('', BookAPIView.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from books.models import Book 
+from .serializers import *
+
+class BookAPIView(generics.ListAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
