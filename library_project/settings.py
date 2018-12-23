b487dc9c8db2151c1251449460c023c88930691d
Kitty <kitsumpang@gmail.com>
2018-12-23 16:53:49 +0000

    added rest framework to settings

--- library_project/settings.py
+++ library_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd Party
+    'rest_framework',
+
+    #Local
     'books.apps.BooksConfig',
 ]
 
