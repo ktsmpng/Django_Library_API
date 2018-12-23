caa482445285d616263a5abcff7500a1e78d0b37
Kitty <kitsumpang@gmail.com>
2018-12-23 16:20:46 +0000

    Rename Books to Book and made migrations

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Books(models.Model):
+class Book(models.Model):
 	title = models.CharField(max_length=250)
 	subtitle = models.CharField(max_length=250)
 	author = models.CharField(max_length=100)
